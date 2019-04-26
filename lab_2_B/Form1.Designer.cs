e73ae2950f35d0f54d21da39090f9e25e817c2a9
MaciejSkrabski <maciej.skrabski@gmail.com>
2019-04-26 08:58:15 +0200

    cosmetics

--- lab_2_B/Form1.Designer.cs
+++ lab_2_B/Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.miniTCPanel2 = new miniTCApp.MiniTCPanel();
-            this.miniTCPanel1 = new miniTCApp.MiniTCPanel();
             this.copyButton = new System.Windows.Forms.Button();
             this.moveButton = new System.Windows.Forms.Button();
             this.deleteButton = new System.Windows.Forms.Button();
             this.pointer = new System.Windows.Forms.Label();
+            this.miniTCPanel2 = new miniTCApp.MiniTCPanel();
+            this.miniTCPanel1 = new miniTCApp.MiniTCPanel();
             this.SuspendLayout();
+            // 
+            // copyButton
+            // 
+            this.copyButton.Location = new System.Drawing.Point(10, 392);
+            this.copyButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.copyButton.Name = "copyButton";
+            this.copyButton.Size = new System.Drawing.Size(129, 46);
+            this.copyButton.TabIndex = 2;
+            this.copyButton.Text = "Kopiuj";
+            this.copyButton.UseVisualStyleBackColor = true;
+            this.copyButton.Click += new System.EventHandler(this.CopyButton_Click);
+            // 
+            // moveButton
+            // 
+            this.moveButton.Location = new System.Drawing.Point(143, 392);
+            this.moveButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.moveButton.Name = "moveButton";
+            this.moveButton.Size = new System.Drawing.Size(125, 46);
+            this.moveButton.TabIndex = 3;
+            this.moveButton.Text = "Przenieś";
+            this.moveButton.UseVisualStyleBackColor = true;
+            this.moveButton.Click += new System.EventHandler(this.MoveButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(273, 391);
+            this.deleteButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(62, 46);
+            this.deleteButton.TabIndex = 4;
+            this.deleteButton.Text = "Usuń";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // pointer
+            // 
+            this.pointer.AutoSize = true;
+            this.pointer.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.pointer.Location = new System.Drawing.Point(332, 195);
+            this.pointer.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.pointer.Name = "pointer";
+            this.pointer.Size = new System.Drawing.Size(37, 26);
+            this.pointer.TabIndex = 5;
+            this.pointer.Text = ">>";
+            this.pointer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // miniTCPanel2
             // 
@@ -43,10 +88,10 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.miniTCPanel2.AutoSize = true;
             this.miniTCPanel2.CurrentPath = "";
-            this.miniTCPanel2.Location = new System.Drawing.Point(488, 12);
-            this.miniTCPanel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.miniTCPanel2.Location = new System.Drawing.Point(366, 10);
+            this.miniTCPanel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.miniTCPanel2.Name = "miniTCPanel2";
-            this.miniTCPanel2.Size = new System.Drawing.Size(434, 464);
+            this.miniTCPanel2.Size = new System.Drawing.Size(326, 377);
             this.miniTCPanel2.TabIndex = 1;
             // 
             // miniTCPanel1
@@ -56,58 +101,17 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.miniTCPanel1.AutoSize = true;
             this.miniTCPanel1.CurrentPath = "";
-            this.miniTCPanel1.Location = new System.Drawing.Point(12, 12);
-            this.miniTCPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.miniTCPanel1.Location = new System.Drawing.Point(9, 10);
+            this.miniTCPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.miniTCPanel1.Name = "miniTCPanel1";
-            this.miniTCPanel1.Size = new System.Drawing.Size(434, 464);
+            this.miniTCPanel1.Size = new System.Drawing.Size(326, 377);
             this.miniTCPanel1.TabIndex = 0;
-            // 
-            // copyButton
-            // 
-            this.copyButton.Location = new System.Drawing.Point(13, 482);
-            this.copyButton.Name = "copyButton";
-            this.copyButton.Size = new System.Drawing.Size(172, 57);
-            this.copyButton.TabIndex = 2;
-            this.copyButton.Text = "📋 Kopiuj";
-            this.copyButton.UseVisualStyleBackColor = true;
-            this.copyButton.Click += new System.EventHandler(this.CopyButton_Click);
-            // 
-            // moveButton
-            // 
-            this.moveButton.Location = new System.Drawing.Point(191, 482);
-            this.moveButton.Name = "moveButton";
-            this.moveButton.Size = new System.Drawing.Size(167, 57);
-            this.moveButton.TabIndex = 3;
-            this.moveButton.Text = "✂️ Przenieś";
-            this.moveButton.UseVisualStyleBackColor = true;
-            this.moveButton.Click += new System.EventHandler(this.MoveButton_Click);
-            // 
-            // deleteButton
-            // 
-            this.deleteButton.Location = new System.Drawing.Point(364, 481);
-            this.deleteButton.Name = "deleteButton";
-            this.deleteButton.Size = new System.Drawing.Size(82, 57);
-            this.deleteButton.TabIndex = 4;
-            this.deleteButton.Text = "🗑";
-            this.deleteButton.UseVisualStyleBackColor = true;
-            this.deleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
-            // 
-            // pointer
-            // 
-            this.pointer.AutoSize = true;
-            this.pointer.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.pointer.Location = new System.Drawing.Point(442, 240);
-            this.pointer.Name = "pointer";
-            this.pointer.Size = new System.Drawing.Size(47, 32);
-            this.pointer.TabIndex = 5;
-            this.pointer.Text = "➡️";
-            this.pointer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(934, 551);
+            this.ClientSize = new System.Drawing.Size(700, 448);
             this.Controls.Add(this.pointer);
             this.Controls.Add(this.deleteButton);
             this.Controls.Add(this.moveButton);
@@ -115,7 +119,7 @@
             this.Controls.Add(this.miniTCPanel2);
             this.Controls.Add(this.miniTCPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
